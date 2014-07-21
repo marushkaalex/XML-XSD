7c755fb2b3622c40ca1a5333d605c378ad9ea0fa
Alexnader Marushka <marushkaalex@gmail.com>
2014-07-21 15:21:25 +0600

    initial commit

---
+++ src/main/java/com/epam/am/Calculate.java
@@ -0,0 +1,7 @@
+package com.epam.am;
+
+public class Calculate {
+    public int calA(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/com/epam/am/Runner.java
@@ -0,0 +1,7 @@
+package com.epam.am;
+
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
