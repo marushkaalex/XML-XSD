e26eea10ee0417139ce0dd0a0fb24d0f0283ba77
marushkaalex <marushkaalex@gmail.com>
2014-07-24 11:55:08 +0600

    entities commit

--- src/main/java/com/epam/am/SaxHandler.java
+++ src/main/java/com/epam/am/SaxHandler.java
@@ -1,16 +1,37 @@
 package com.epam.am;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SaxHandler extends DefaultHandler {
+
+    String temp;
+
     @Override
     public void startDocument() throws SAXException {
-        System.out.println("start document");
+        System.out.println("Tariffs info");
     }
 
     @Override
     public void endDocument() throws SAXException {
-        System.out.println("end document");
+        System.out.println("<end of the document>");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.println("<" + qName + ">");
+        temp = "";
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        System.out.println(temp);
+        System.out.println("</" + qName + ">");
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        temp = new String(ch, start, length);
     }
 }
